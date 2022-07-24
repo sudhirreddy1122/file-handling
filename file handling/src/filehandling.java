3731e593d997630571033884d5861b112b4be08e
sudhirreddy1122 <sudhir17072000@gmail.com>
2022-07-24 18:26:40 +0530

    file handling

--- file handling/src/filehandling.java
+++ file handling/src/filehandling.java
@@ -1,9 +1,23 @@
 
+
+import java.io.File;
+import java.io.IOException;
+
 public class filehandling {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		File myfile = new File("FileHandling.txt");
+		
+		try {
+			if (myfile.createNewFile()) {
+				System.out.println("File Created Successfully");
+			}
+			else {
+				System.out.println("File Not Created");
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
-
 }
